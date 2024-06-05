b2c396495732a87dad137ba19ca968a1ee59bca9
Simplerdevs <tplg7839@gmail.com>
2024-06-05 09:05:14 -0700

    Add project files.

---
+++ Problem #7 ( Half Number )/Problem #7 ( Half Number ).cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+
+{
+    /* program to ask the user to enter number then print the half of the inputs */
+
+    // inputs
+    int num1 = 60 , num2 = 50;
+
+    // outputs
+    cout << "Half of the "  << num1 << " is " << num1 / 2 << endl;
+    cout << "Half of the " << num2 << " is " << num2 / 2 << endl;
+
+    return 0;
+}
+
